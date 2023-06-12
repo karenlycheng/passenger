10352bb4e7f156c9ce7afed2088a896c80ff1577
karenlycheng <51065328+karenlycheng@users.noreply.github.com>
2023-06-12 19:51:13 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 let countEl = document.getElementById("count-el")
 let count = 0
 let countSave = document.getElementById("count-save")
+let countTotal = document.getElementById("count-total")
 
 function increment() {
     count += 1
@@ -19,5 +20,8 @@ function save() {
     let countString = count + " - "
     countSave.textContent += countString 
     countEl.textContent = 0
+    let runningTotal = count 
+    countTotal.textContent = runningTotal
+   
     count = 0
 }
